f8575ac979e0e9b6e69a71701d6266b34b2b604a
paolagc <paola@usertime.se>
2015-02-26 10:11:12 -0500

    fix archive template

--- archive.php
+++ archive.php
@@ -13,26 +13,28 @@
 							the_archive_title( '<h2 class="page-title">', '</h2>' );
 							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
-						<?php while ( have_posts() ) : the_post(); ?>
-							<div class="row blog blog-medium margin-bottom-40">
-								<?php if ( has_post_thumbnail() ) : ?>
-									<div class="col-md-5">
-										<?php the_post_thumbnail('medium'); ?>
-									</div>
-									<div class="col-md-<?php print ( has_post_thumbnail())? 7 : 12 ?>">
-										<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-										<ul class="list-unstyled list-inline blog-info">
-									        <li><i class="fa fa-calendar"></i> <?php the_date('Y-m-d'); ?></li>
-									        <li><i class="fa fa-pencil"></i> <?php the_author_link(); ?></li>
-									        <li><i class="fa fa-comments"></i> <a href="#"> <?php comments_number( '0 comments', '1 comment', '% comments' ); ?>.</a></li>
-									    </ul>
-									    <?php the_excerpt(); ?> 
-									    <a href="<?php echo get_permalink( ); ?>" class="btn-u btn-u-sm">Read More<i class="fa fa-angle-double-right"></i></a>
-									</div>
-								<?php endif; ?>
-							</div>
-							<hr>
-						<?php endwhile; // end of the loop. ?>
+						<?php if ( have_posts() ) : ?>
+							<?php while ( have_posts() ) : the_post(); ?>
+								<div class="row blog blog-medium margin-bottom-40">
+									<?php if ( has_post_thumbnail() ) : ?>
+										<div class="col-md-5">
+											<?php the_post_thumbnail('medium'); ?>
+										</div>
+									<?php endif; ?>
+										<div class="col-md-<?php print ( has_post_thumbnail())? 7 : 12 ?>">
+											<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+											<ul class="list-unstyled list-inline blog-info">
+										        <li><i class="fa fa-calendar"></i> <?php the_date('Y-m-d'); ?></li>
+										        <li><i class="fa fa-pencil"></i> <?php the_author_link(); ?></li>
+										        <li><i class="fa fa-comments"></i> <a href="#"> <?php comments_number( '0 comments', '1 comment', '% comments' ); ?>.</a></li>
+										    </ul>
+										    <?php the_excerpt(); ?> 
+										    <a href="<?php echo get_permalink( ); ?>" class="btn-u btn-u-sm">Read More<i class="fa fa-angle-double-right"></i></a>
+										</div>
+								</div>
+								<hr>
+							<?php endwhile; // end of the loop. ?>
+						<?php endif; ?>
 					</section>
 					<?php get_sidebar(); ?>
 			</div>
--- front-page.php
+++ front-page.php
@@ -52,14 +52,18 @@ wp_reset_query();?>
 	</section>
 <?php endif; ?>
 
+
 <section id="main-content" role="main">
 <?php query_posts(array('post_type' => 'showcase','orderby' => 'name'));
  if(have_posts()) : while(have_posts()) : the_post();
 	$items[] = array(
 				'title' => get_the_title(),
-				'caption' => get_post_meta($post->ID, 'slider_caption', true), 
-				'image' => get_post_meta($post->ID, 'slider_image', true),
-				'url' => get_post_meta($post->ID, 'slider_url', true),
+				'company' => get_post_meta($post->ID, 'showcase_company', true), 
+				'caption' => get_post_meta($post->ID, 'showcase_caption', true),
+				'company' => get_post_meta($post->ID, 'showcase_features', true), 
+				'caption' => get_post_meta($post->ID, 'showcase_date', true),
+				'company' => get_post_meta($post->ID, 'showcase_image', true),
+				'url' => get_post_meta($post->ID, 'showcase_url', true),
 			);
 		endwhile; 
 	endif; 
